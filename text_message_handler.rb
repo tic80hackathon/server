c470b41ff4bcbec9e0dc5d575e12574c593afdf6
mathias aboudou <mathias.aboudou@gmail.com>
2018-11-02 17:42:47 +0900

    debug bot message again...

--- text_message_handler.rb
+++ text_message_handler.rb
@@ -59,6 +59,7 @@ class TextMessageHandler
     end
 
     def latest_cartridges2(event, cartridges)
+        c = cartridges[0]
         reply_content(event, {
           type: 'template',
           altText: 'Carousel alt text',
@@ -66,45 +67,12 @@ class TextMessageHandler
             type: 'carousel',
             columns: [
               {
-                title: 'hoge',
-                text: 'fuga',
+                title: c.name,
+                text: c.description,
                 actions: [
                   { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
                   { label: 'Send postback', type: 'postback', data: 'hello world' },
                   { label: 'Send message', type: 'message', text: 'This is message' }
-                ]
-              },
-              {
-                title: 'Datetime Picker',
-                text: 'Please select a date, time or datetime',
-                actions: [
-                  {
-                    type: 'datetimepicker',
-                    label: "Datetime",
-                    data: 'action=sel',
-                    mode: 'datetime',
-                    initial: '2017-06-18T06:15',
-                    max: '2100-12-31T23:59',
-                    min: '1900-01-01T00:00'
-                  },
-                  {
-                    type: 'datetimepicker',
-                    label: "Date",
-                    data: 'action=sel&only=date',
-                    mode: 'date',
-                    initial: '2017-06-18',
-                    max: '2100-12-31',
-                    min: '1900-01-01'
-                  },
-                  {
-                    type: 'datetimepicker',
-                    label: "Time",
-                    data: 'action=sel&only=time',
-                    mode: 'time',
-                    initial: '12:15',
-                    max: '23:00',
-                    min: '10:00'
-                  }
                 ]
               }
             ]
